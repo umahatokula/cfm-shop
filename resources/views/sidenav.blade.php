546a3d5e612a775304b29a678eb8c05658fba7a6
Umaha <umahatokula@example.com>
2019-07-19 07:00:44 +0100

    Initial commit

---
+++ resources/views/sidenav.blade.php
@@ -0,0 +1,22 @@
+<div class="side-header show">
+    <button class="side-header-close"><i class="zmdi zmdi-close"></i></button>
+    <!-- Side Header Inner Start -->
+    <div class="side-header-inner custom-scroll">
+
+        <nav class="side-header-menu" id="side-header-menu">
+            <ul>
+                <li class="has-sub-menu"><a href="#"><i class="ti-home"></i> <span>Dashboard</span></a>
+                </li>
+                <li><a href="#"><i class="ti-palette"></i> <span>Categories</span></a></li>
+                <li class="has-sub-menu"><a href="#"><i class="ti-package"></i> <span>System Settings</span></a>
+                    <ul class="side-header-sub-menu">
+                        <li><a href="elements-alerts.html"><span>Setting 1</span></a></li>
+                        <li><a href="elements-accordions.html"><span>Setting 2</span></a></li>
+                    </ul>
+                </li>
+
+            </ul>
+        </nav>
+
+    </div><!-- Side Header Inner End -->
+</div>
