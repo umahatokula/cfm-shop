e31f4a04b1f684707f739f374d326f11dacd8824
Imoni Christopher <imonireal@gmail.com>
2019-07-25 21:49:46 +0100

    implement CRUD api for categories

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,3 @@ Route::get('roles', 'RolesController@getRoles')->name('roles');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/about', function() {
-    return 1234;
-});
